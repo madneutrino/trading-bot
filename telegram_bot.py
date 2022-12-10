f6a9781f1b60debf393f6f127aa602b4331a2789
psyf <gzsaif001@gmail.com>
2022-12-10 19:43:11 +0800

    fix telegram logging problems

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,8 +6,6 @@ from dotenv import load_dotenv
 from parse_call import TradingCallParser
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import logging
-import sys
 from utils import setup_logger
 
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
@@ -52,18 +50,18 @@ def filter_and_save(message):
                 if not is_duplicate(new_call):
                     session.add(new_call)
                     session.commit()
-                    logging.info("New call => " + str(new_call))
+                    logger.info("New call => " + str(new_call))
             except Exception as e:
-                logging.error("Could not parse call => " + str(message.id) + str(e))
+                logger.error("Could not parse call => " + str(message.id) + str(e))
         else:
-            logging.debug("Already exists => " + str(message.id))
+            logger.debug("Already exists => " + str(message.id))
     else:
         if message.reply_to_msg_id:
             orig_call = session.query(TradingCall).get(message.reply_to_msg_id)
             if orig_call is not None and orig_call.bragged == 0:
                 orig_call.bragged = 1
                 session.commit()
-                logging.info(f"Bragged/Cancelled => {message.reply_to_msg_id}")
+                logger.info(f"Bragged/Cancelled => {message.reply_to_msg_id}")
 
 
 # for debouncing duplicate calls
